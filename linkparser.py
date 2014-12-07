47743a796e8f7344a6a8e7044b2e80847c46f2d9
deadsoft <butkus.algirdas@gmail.com>
2014-12-07 11:57:32 +0200

    update

--- linkparser.py
+++ linkparser.py
@@ -16,7 +16,7 @@
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-version = 0.014
+version = 0.015
 
 from PyQt4 import QtCore
 
